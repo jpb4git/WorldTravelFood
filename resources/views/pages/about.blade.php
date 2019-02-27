2604241d7982014b320a6bf6c2fe63473e2cd8be
Jean-Pascal Boudet <dev@guitaresphere.com>
2019-02-27 15:14:39 +0100

    first commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>About</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+
+</head>
+<body>
+<h1>About</h1>
+</body>
+</html>
