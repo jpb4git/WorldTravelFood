85e11391e4fb9f06ee4bfad7e3fbbcdb629c0f93
QuemenerJohanne <quemener.johanne@gmail.com>
2019-02-28 16:22:19 +0100

    header+links

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app', ['title' => 'Home'])
 @section('content')
-       <h1>The Laravel Welcome Page {{$name}}</h1>
+
 @endsection
 
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,8 +1,11 @@
-
 <style>
 
     .orange {
-        background-color: red !important;
+        background-color: #dd352e !important;
+    }
+
+    .blue {
+        background-color: #67c7c6 !important;
     }
 
 </style>
@@ -10,10 +13,29 @@
 
 <header>
 
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col">
-                Livraison offerte dés 50€ d'achat !!
+    <div class="container-fluid orange">
+
+        <div class="row ">
+            <div class="col text-white text-center p-2">
+                <i class="fas fa-shipping-fast"></i> Livraison offerte dés 50€ d'achat !!
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid blue p-3">
+
+        <div class="row ">
+            <div class="col-lg-3">
+                <div class="input-group d-flex justify-content-start">
+                    <input type="text" class="form-control" placeholder="recherche">
+                    <div class="input-group-append">
+                        <button class="btn btn-secondary" type="button">
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </div>
+                </div>
             </div>
 
+    </div>
+    </div>
 </header>
