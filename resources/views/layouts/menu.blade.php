c43e2cedd09f88fff95d7e5f8b636c7459f1ae8c
Arthur <arthurproust1@gmail.com>
2019-02-28 16:22:27 +0100

    Style(menu) : Improve menu style

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,42 @@
+<style>
+    hr.style1 {
+        border-top: 8px solid #67c7c6;
+    }
+
+</style>
+
+
+<div class="container-fluid navbar-center">
+    <br>
+    <hr class="style1">
+    <br>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">WTF</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse navbar-center" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/">ACCUEIL <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('product.showAll') }}" class="nav-link">BOUTIQUE</a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('pays.showAll') }}" class="nav-link">PAYS</a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('cat.showAll') }}" class="nav-link">CATÉGORIES</a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('concept.showAll') }}" class="nav-link">CONCEPT</a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('contact.showAll') }}" class="nav-link">CONTACT</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
