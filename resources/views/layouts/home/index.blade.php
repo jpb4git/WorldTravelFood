0f0e19e6fc751f2d3d9b4b9f2c3f4c72f6612cba
Jean-Pascal Boudet <dev@guitaresphere.com>
2019-02-28 17:20:09 +0100

    add carous index blade

--- resources/views/layouts/home/index.blade.php
+++ resources/views/layouts/home/index.blade.php
@@ -10,9 +10,17 @@
         .img-car {
             height: 500px;
         }
+
+        .wave{
+            position:absolute;
+            width:100% ;
+            height: 100%;
+            background-color: #4e555b;
+            opacity: 0.5;
+        }
     </style>
 
-    <div class="container">
+    <div class="container mt-5">
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -21,21 +29,21 @@
             </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="{{asset('assets/images/car-1.jpeg')}}" class="d-block w-100 img-fluid img-car" alt="...">
+                    <img src="{{asset('assets/images/car-1.jpg')}}" class="d-block w-100 img-fluid img-car" alt="...">
                     <div class="carousel-caption d-none d-md-block">
                         <h5>Test</h5>
                         <p>caption n 1 </p>
                     </div>
                 </div>
                 <div class="carousel-item">
-                    <img src="{{asset('assets/images/car-2.jpeg')}}" class="d-block w-100 img-fluid img-car" alt="...">
+                    <img src="{{asset('assets/images/car-2.jpg')}}" class="d-block w-100 img-fluid img-car" alt="...">
                     <div class="carousel-caption d-none d-md-block">
                         <h5>Test</h5>
-                        <p>caption n 2 </p>
+                        <p>caption n 2</p>
                     </div>
                 </div>
                 <div class="carousel-item">
-                    <img src="{{asset('assets/images/car-4.jpeg')}}" class="d-block w-100 img-fluid img-car" alt="...">
+                    <img src="{{asset('assets/images/car-3.jpg')}}" class="d-block w-100 img-fluid img-car" alt="...">
                     <div class="carousel-caption d-none d-md-block">
                         <h5>Test</h5>
                         <p>caption n 3 </p>
@@ -58,13 +66,23 @@
             <div class="col-md-4 text-center">
                 <div class="wrapper">
                     <div class="wave"></div>
-                    <div><img src="" alt="">
+                    <div><img class="w-50 img-fluid"src="{{asset('assets/images/gamme.jpg')}}" alt="">
                     </div>
                 </div>
             </div>
-            <div class="col-md-4 text-center">T
+            <div class="col-md-4 text-center">
+                <div class="wrapper">
+                    <div class="wave"></div>
+                    <div><img class="w-50 img-fluid"src="{{asset('assets/images/gamme.jpg')}}" alt="">
+                    </div>
+                </div>
             </div>
-            <div class="col-md-4 text-center">T
+            <div class="col-md-4 text-center m">
+                <div class="wrapper">
+                    <div class="wave "></div>
+                    <div><img class="w-50 img-fluid"src="{{asset('assets/images/gamme.jpg')}}" alt="">
+                    </div>
+                </div>
             </div>
         </div>
     </div>
