b15cdb83ab6e071eb4d854c1a2d23f8bb61e2bbe
marlenegomes <marlenegomes1@gmail.com>
2019-03-04 15:46:48 +0100

    modification footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,44 +1,77 @@
+<style>
+
+</style>
+
 <footer>
-    <div class="container-fluid ">
+    <div class="container-fluid blue p-4 mt-5">
         <div class="row no-gutters">
+            <div class="col-12, col-md-6, col-lg-3  ">
+                <a href="">
+                    <i class="fab fa-cc-paypal "></i>
+                    <i class="fab fa-cc-mastercard"></i>
+                    <i class="fab fa-cc-amex"></i>
+                    <i class="fab fa-cc-visa"></i>
+                    Paiement sécurisé
+                </a>
+            </div>
             <div class="col-12, col-md-6, col-lg-3">
-                <a href=""><i class="fab fa-cc-paypal"></i><i class="fa-cc-mastercard"></i><i class="fa-cc-amex"></i>Paiement sécurisé</a>
+                <a href="">
+                    <i class="fas fa-shopping-bag"></i>
+                    Take away
+                </a>
             </div>
             <div class="col-12, col-md-6, col-lg-3">
                 <a>
-                    <img src="" alt="icone sac/magasin">
+                    <img class="logo" src="{{asset('assets/images/logo-WTF-vertical.png')}}" alt="logo world travel food">
                 </a>
-                <a href="">Take away</a>
+                <a href="">Service client</a>
             </div>
-            <div class="col-12, col-md-6, col-lg-3">
-                <a>
-                    <img src="" alt="logo/mascotte">
-                </a>
-                <a href="">Service client Informations</a>
-            </div>
+
         </div>
         <div class="row no-gutters">
             <div class="col-12, col-md-6, col-lg-3">
-                <a>
-                    <img src="" alt="icone livraison">
+                <a href="" >
+                    <i class="fas fa-shipping-fast"></i>
+                    Livraison sous 24 heures
                 </a>
-                <a href="">Livraison sous 24 heures</a>
             </div>
             <div class="col-12, col-md-6, col-lg-3">
-                <a>
-                    <img src="" alt="icones courrier/tel">
+                <a href="">
+                    <i class="fas fa-envelope"></i>
+                    Contact
                 </a>
-                <a href="">Contact</a>
             </div>
             <div class="col-12, col-md-6, col-lg-3">
-
-                <form class="form-inline ">
-                    <label>S'inscrire à la newsletter</label>
-                    <input type="email" class="form-control" name="mail" placeholder="email"type="text">
-                    <button type="submit" name="submitNewsletter" class="badge-success">OK</button>
+                <form action="" method="post">
+                    <div class="form-group">
+                        <label for=mail">S'inscrire à la newsletter</label>
+                        <input type="email" class="form-control" name="mail" placeholder="email">
+                        <button type="submit" name="submitNewsletter" class="btn btn-success">OK</button>
+                    </div>
                 </form>
-
             </div>
+        </div>
+        <div class="row d-line">
+            <a href="">
+                <img class="RSociaux" src="{{asset('assets/images/facebook.png')}}" alt="logo facebook">
+            </a>
+            <a href="">
+                <img class="RSociaux" src="{{asset('assets/images/instagram.png')}}" alt="logo instagram">
+            </a>
+            <a href="">
+                <img class="RSociaux" src="{{asset('assets/images/youtube.png')}}" alt="logo youtube">
+            </a>
+            <a href="">
+                <img class="RSociaux" src="{{asset('assets/images/twitter.png')}}" alt="logo twitter">
+            </a>
+            <a href="">
+                <img class="RSociaux" src="{{asset('assets/images/pinterest.png')}}" alt="logo pinterest">
+            </a>
+        </div>
+        <div class="row d-inline ">
+            <a href="" class="p-5">Conditions générales de vente</a>
+            <a href="" class="p-5">Mentions légales</a>
+            <a href="" class="p-5">Politique de confidentialité</a>
         </div>
     </div>
 </footer>
