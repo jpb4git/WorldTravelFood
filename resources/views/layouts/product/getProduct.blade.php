fe794db9bf7579616d4056a2513f8ec1811b2cb6
Jean-Pascal Boudet <dev@guitaresphere.com>
2019-03-04 12:45:06 +0100

    produit modif

--- resources/views/layouts/product/getProduct.blade.php
+++ resources/views/layouts/product/getProduct.blade.php
@@ -1,82 +1,8 @@
 @extends('layouts.app', ['title' => 'Fiche Produit'])
 
 @section('content')
-    <style>
-        .orange {
-            background-color: #dd352e !important;
-        }
-
-        .orange-Text {
-            color: #dd352e !important;
-        }
-
-        .blue {
-            background-color: #67c7c6 !important;
-        }
-
-        .grey {
-            background-color: #a5babc !important;
-        }
-
-        .grey-Text {
-            color: #a5babc !important;
-        }
 
 
-        .stars {
-            width: 1em;
-            height: 1em;
-        }
-
-        .size-Product {
-            width: 300px;
-            height: 300px;
-        }
-
-        .description {
-            font-size: 0.7em;
-        }
-
-        .provenance {
-            font-size: 0.6em;
-        }
-
-        .row-breadcrumb {
-            padding: 0;
-            margin: 0 auto;
-        }
-
-        .ul-nav {
-            list-style-type: none;
-        }
-
-        .right-price {
-            border-right-style: solid;
-        }
-
-        .content-price {
-
-        content_prices .left {
-            width: 157px;
-            text-align: center;
-            border-right: 3px solid;
-        }
-
-         .spot {
-
-            width: 125px!important;
-            height: 125px!important;
-            border-radius: 50%!important;
-
-        }
-
-         .spotAlert {
-            border: 1px solid red;
-            background-color: red;
-        }
-
-        }
-    </style>
     <div class="container-fluid ">
         <div class="row row-breadcrumb ">
             <div class="col-md-12 ">
@@ -91,23 +17,22 @@
     </div>
 
     {{--    PRODUCT    --}}
-    <div class="container ">
+    <div class="container">
 
         <div class="row ">
             <div class="col-md-4">
                 <img class="size-Product img-fluid" src="{{asset('assets/images/gamme.jpg')}}" alt="">
             </div>
             <div class="col-md-8">
-                <div class="wrapper mb-5">
+                <div class="wrapper mb-5" id="warp-stars">
                     <span><strong>VIN DE COBRA & SCORPION - GRANDE BOUTEILLE</strong></span>
                     <div class="wrapper-stars">
-                        <img class="stars img-fluid" src="{{asset('assets/images/starOn.svg')}}" alt="">
-                        <img class="stars img-fluid" src="{{asset('assets/images/starOn.svg')}}" alt="">
-                        <img class="stars img-fluid" src="{{asset('assets/images/starOn.svg')}}" alt="">
-                        <img class="stars img-fluid" src="{{asset('assets/images/starOn.svg')}}" alt="">
-                        <img class="stars img-fluid" src="{{asset('assets/images/starOff.svg')}}" alt="">
-                        <span><a class="orange-Text" href=""> 3 commantaire</a></span> | <span><a class="orange-Text"
-                                                                                                  href="">Ecrire un commentaire</a></span>
+                        <img class="star-size" src="{{asset('assets/images/starOn.svg')}}" alt="">
+                        <img class="star-size" src="{{asset('assets/images/starOn.svg')}}" alt="">
+                        <img class="star-size" src="{{asset('assets/images/starOn.svg')}}" alt="">
+                        <img class="star-size" src="{{asset('assets/images/starOn.svg')}}" alt="">
+                        <img class="star-size" src="{{asset('assets/images/starOff.svg')}}" alt="">
+                        <span><a class="text-primary" href=""> 3 commentaire</a></span> | <span><a class="text-primary" href="">Ecrire un commentaire</a></span>
                     </div>
                     <hr>
 
@@ -139,9 +64,9 @@
 
                     </div>
                 </div>
-                <div class="row-fluid bg-light ">
+                <div class="row-fluid bg-light">
                     <div class="col-md-12 ">
-                        <span class="spot spotAlert">7</span>
+                        <span class="spot bg-primary">7</span>
                         <span class=" text-center bg-danger">En rupture de Sotck</span>
                     </div>
                 </div>
