41804fe6447f358d4c3c779cc7a509fb37d7af2e
Marlène Gomes <marlenegomes1@gmail.com>
2019-03-04 23:16:46 +0100

    Modification style footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,77 +1,71 @@
-<style>
-
-</style>
-
-<footer>
-    <div class="container-fluid blue p-4 mt-5">
-        <div class="row no-gutters">
-            <div class="col-12, col-md-6, col-lg-3">
-                <a href="">
-                    <i class="fab fa-cc-paypal "></i>
-                    <i class="fab fa-cc-mastercard"></i>
-                    <i class="fab fa-cc-amex"></i>
-                    <i class="fab fa-cc-visa"></i>
-                    Paiement sécurisé
-                </a>
-            </div>
-            <div class="col-12, col-md-6, col-lg-3">
-                <a href="">
-                    <i class="fas fa-shopping-bag"></i>
-                    Take away
-                </a>
-            </div>
-            <div class="col-12, col-md-6, col-lg-3">
-                <a>
-                    <img class="logo" src="{{asset('assets/images/logo-WTF-vertical.png')}}" alt="logo world travel food">
-                </a>
-                <a href="">Service client</a>
-            </div>
-
-        </div>
-        <div class="row no-gutters">
-            <div class="col-12, col-md-6, col-lg-3">
-                <a href="" >
-                    <i class="fas fa-shipping-fast"></i>
-                    Livraison sous 24 heures
-                </a>
-            </div>
-            <div class="col-12, col-md-6, col-lg-3">
-                <a href="">
-                    <i class="fas fa-envelope"></i>
-                    Contact
-                </a>
-            </div>
-            <div class="col-12, col-md-6, col-lg-3">
-                <form action="" method="post">
-                    <div class="form-inline">
-                        <label for=mail">S'inscrire à la newsletter</label>
-                        <input type="email" class="form-control" name="mail" placeholder="email">
-                        <button type="submit" name="submitNewsletter" class="btn btn-success">OK</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-        <div class="row d-line">
+<footer class="container-fluid blue p-4">
+    <div class="avantages row no-gutters row no-gutters justify-content-center text-center pb-3">
+        <div class="col-12, col-md-6, col-lg-3">
             <a href="">
-                <img class="RSociaux" src="{{asset('assets/images/facebook.png')}}" alt="logo facebook">
-            </a>
-            <a href="">
-                <img class="RSociaux" src="{{asset('assets/images/instagram.png')}}" alt="logo instagram">
-            </a>
-            <a href="">
-                <img class="RSociaux" src="{{asset('assets/images/youtube.png')}}" alt="logo youtube">
-            </a>
-            <a href="">
-                <img class="RSociaux" src="{{asset('assets/images/twitter.png')}}" alt="logo twitter">
-            </a>
-            <a href="">
-                <img class="RSociaux" src="{{asset('assets/images/pinterest.png')}}" alt="logo pinterest">
+                <i class="fab fa-cc-paypal "></i>
+                <i class="fab fa-cc-mastercard"></i>
+                <i class="fab fa-cc-amex"></i>
+                <i class="fab fa-cc-visa"></i>
+                Paiement sécurisé
             </a>
         </div>
-        <div class="row d-inline">
-            <a href="" class="p-5">Conditions générales de vente</a>
-            <a href="" class="p-5">Mentions légales</a>
-            <a href="" class="p-5">Politique de confidentialité</a>
+        <div class="col-12, col-md-6, col-lg-3">
+            <a href="">
+                <i class="fas fa-shopping-bag"></i>
+                Take away
+            </a>
         </div>
+        <div class="col-12, col-md-6, col-lg-3">
+            <a href="" >
+                <i class="fas fa-shipping-fast"></i>
+                Livraison sous 24 heures
+            </a>
+        </div>
+        
+    </div>
+    <div class="row no-gutters justify-content-center text-center">
+        <div class="col-12, col-md-6, col-lg-3">
+            <a>
+                <img class="logo" src="{{asset('assets/images/logo-WTF-vertical.png')}}" alt="logo world travel food">
+            </a>
+            <a href="">Service client</a>
+        </div>
+        <div class="col-12, col-md-6, col-lg-3">
+            <a href="">
+                <i class="fas fa-envelope"></i>
+                Contact
+            </a>
+        </div>
+        <div class="col-12, col-md-6, col-lg-3">
+            <form action="" method="post">
+                <div class="form-inline">
+                    <label for="mail" class="mr-3">S'inscrire à la newsletter</label>
+                    <input type="email" class="form-control" name="mail" placeholder="email">
+                    <button type="submit" name="submitNewsletter" class="btn btn-success">OK</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="row justify-content-center py-4">
+        <a href="" class="mx-2">
+            <img class="RSociaux" src="{{asset('assets/images/facebook.png')}}" alt="logo facebook">
+        </a>
+        <a href="" class="mx-2">
+            <img class="RSociaux" src="{{asset('assets/images/instagram.png')}}" alt="logo instagram">
+        </a>
+        <a href="" class="mx-2">
+            <img class="RSociaux" src="{{asset('assets/images/youtube.png')}}" alt="logo youtube">
+        </a>
+        <a href="" class="mx-2">
+            <img class="RSociaux" src="{{asset('assets/images/twitter.png')}}" alt="logo twitter">
+        </a>
+        <a href="" class="mx-2">
+            <img class="RSociaux" src="{{asset('assets/images/pinterest.png')}}" alt="logo pinterest">
+        </a>
+    </div>
+    <div class="legal row justify-content-center">
+        <a href="" class="px-5 py-2">Conditions générales de vente</a>
+        <a href="" class="px-5 py-2">Mentions légales</a>
+        <a href="" class="px-5 py-2">Politique de confidentialité</a>
     </div>
 </footer>
