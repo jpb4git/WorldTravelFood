784d9f04595cedcc57091a36dec90529f3f840be
jip <dev@guitaresphere.Com>
2019-03-10 12:30:03 +0100

    Admin design add Droits link and dashboarded

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -100,6 +100,22 @@
                     </div>
                 </div>
             </div>
+            <div class="col-md-4">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <i class="far fa-address-book big-icon"></i>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Utilisateurs : <strong>4</strong></h5>
+                                <p class="card-text">Utilisateurs presents dans la base de données.</p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         <hr>
         <div class="row mt-1 mb-5">
