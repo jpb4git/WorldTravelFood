6f442bedfba88f8ea709c2be317660ab292bf1eb
marlenegomes <marlenegomes1@gmail.com>
2019-03-14 09:11:03 +0100

    update order_products_table and databaseSeeder

--- database/migrations/2019_03_11_141957_create_order_products_table.php
+++ database/migrations/2019_03_11_141957_create_order_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOrderProductTable extends Migration
+class CreateOrderProductsTable extends Migration
 {
     /**
      * Run the migrations.
