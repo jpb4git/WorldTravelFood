392e54772e1bfe888716282a3f64cfb387880d4e
xkr225 <yn.michel@gmail.com>
2019-04-05 15:34:23 +0200

    maj routes

--- routes/web.php
+++ routes/web.php
@@ -52,14 +52,11 @@ Route::get('/produits/country', 'ProductController@country')->name('product.coun
  */
 Route::get('/panier', 'CartController@index')->name('panier.index');
 
-Route::get('/panier/createAccount', 'CartController@createAccount')->name('panier.account');
-
 Route::get('/panier/adresse1', 'CartController@adresse1')->name('panier.adresse1');
 
 Route::get('/panier/adresse2', 'CartController@adresse2')->name('panier.adresse2');
 
-
-Route::get('/panier/fraisdeport', 'CartController@fraisDePort')->name('panier.shipping');
+Route::get('/panier/fraisdeport', 'CartController@shipping')->name('panier.shipping');
 
 Route::get('/panier/paiement', 'CartController@paiement')->name('panier.payment');
 
@@ -73,7 +70,7 @@ Route::get('/panier/paiement', 'CartController@paiement')->name('panier.payment'
  *
  *
  */
-Route::get('/order', 'OrderController@show');
+Route::get('/order', 'OrderController@index')->name('order.index');
 
 
 
